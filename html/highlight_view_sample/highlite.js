42df5ed7676fdd13e787c3e73f83e72f5c752c3c
tetsustet <50937719+tetsustet@users.noreply.github.com>
2021-10-28 01:02:04 +0900

    fix locationHighliteWrapper problem

--- html/highlight_view_sample/highlite.js
+++ html/highlight_view_sample/highlite.js
@@ -5,4 +5,13 @@ $(window).on("load resize",function(){
 function init(){
     console.log($(".photo:first").height());
     $(".photo").width($(".photo").height());
+    $(".photo").css("padding", $(".photo").height() * 0.01);
+    $(".locationHighlite").each(function(index, element){
+        $(this).css("width", $(this).children(".photos").width());
+    });
+}
+
+let style;
+
+function setup() {
 }
