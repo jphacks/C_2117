b23bb77d1063332dcd30a66ca1f03d96eeb679e1
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-10-28 23:47:16 +0900

    フィルター

--- server/trip_diary/urls.py
+++ server/trip_diary/urls.py
@@ -6,7 +6,6 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'images', views.ImageViewSet)
-router.register(r'images/<int:user_id>', views.ImageViewSet)
 urlpatterns = [
     path('', views.index, name='index'),
 ]
--- server/trip_diary/views.py
+++ server/trip_diary/views.py
@@ -6,9 +6,9 @@ from .models import Image
 from .serializer import ImageSerializer
 
 class ImageViewSet(viewsets.ModelViewSet):
-    model = Image
     queryset = Image.objects.all()
     serializer_class = ImageSerializer
+    filter_fields = ('user_id', 'trip_id')
 
 
 def index(request):
