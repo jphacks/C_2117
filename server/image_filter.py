6a348c88a09cdefa182d3b3492913c347601ae6f
Tomoki-sic <cy17237@shibaura-it.ac.jp>
2021-10-30 03:13:03 +0900

    test

--- server/image_filter.py
+++ server/image_filter.py
@@ -1,4 +1,3 @@
-import cv2
 import glob
 import os
 
@@ -10,8 +9,25 @@ images = glob.glob('*.JPG')
 os.chdir("./test")
 i=0
 for fname in images:
-    img = cv2.imread(fname)
+    os.chdir("..")
+    i+=1
+    print(fname)
+    # 画像ファイルを開く
+    im = Image.open(fname)
+    exif = im._getexif()
+    dic_ = {}
+    date=str(i)
+    tag = str(i)
+    for id, value in exif.items():
+        dic_[TAGS.get(id)] = value
+        if TAGS.get(id) == "DateTimeOriginal":
+            year = dic_['DateTimeOriginal'].split(' ')[0].split(':')[0]
+            date = str(i)+"_Original_"+dic_['DateTimeOriginal'].split(' ')[0].split(':')[1] + '-' + dic_['DateTimeOriginal'].split(' ')[0].split(':')[2]
+            continue
+        if date==str(i) and TAGS.get(id) == "DateTimeDigitized":
+            tag = str(i)+"_Digitized_"+dic_['DateTimeDigitizedl']
 
-
+    os.chdir("./test")
+    im.save(date+".jpg")
 
 
