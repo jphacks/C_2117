1b37df468061914f3ad6b796259b4e662c9dcb68
KoheiIida <cy18204@shibaura-it.ac.jp>
2021-10-29 11:08:52 +0900

    location

--- html/trip_selection_view/trip_selection.js
+++ html/trip_selection_view/trip_selection.js
@@ -1,13 +1,41 @@
-$("window").on("load",function(){
+$(window).on("load",function(){
     init();
 })
 
 function init(){
+    const swiper = new Swiper('#tripselectioncontainer', {
+        centeredSlides: true,
+        loop: true,
+        speed: 500,
+        slidesPerView: 1.5,
+        spaceBetween: 40,
+        autoplay: {
+            delay: 3000,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+    
+            640: {
+                slidesPerView: 2.5,
+            },
+            768: {
+                slidesPerView: 2.75,
+            },
+            1080: {
+                slidesPerView: 3.25,
+            },
+            1280: {
+                slidesPerView: 3.75,
+            },
+        },
+    });
 }
 
-$(function(){
-    $(".group-menu").hide();
-    $(".button").click(function(){
-        $(".group-menu").toggle(300);
-    });
-});
+
+// $(".group-menu").hide();
+// $(".button").click(function(){
+//     $(".group-menu").toggle(300);
+// });
