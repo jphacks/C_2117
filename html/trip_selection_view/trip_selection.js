84135945270c3f3bb7b9103a60b67ce577224dac
KoheiIida <cy18204@shibaura-it.ac.jp>
2021-10-31 00:17:30 +0900

    tripselection server connect

--- html/trip_selection_view/trip_selection.js
+++ html/trip_selection_view/trip_selection.js
@@ -1,4 +1,7 @@
 let swiper;
+let trips;
+let images;
+const api_prefix ="http://127.0.0.1:8000/"
 $(window).on("load",function(){
     init();
 })
@@ -6,6 +9,29 @@ $(window).on("load",function(){
 
 
 function init(){
+    photoInit();
+
+    let requestURL_trip = 'http://127.0.0.1:8000/api/trip';
+    let request_trip = new XMLHttpRequest();
+    request_trip.open('GET', requestURL_trip);
+    request_trip.responseType = 'json';
+    request_trip.send();
+    request_trip.onload = function() {
+        console.log(request_trip.response);
+        trips = request_trip.response;
+        let requestURL_images = 'http://127.0.0.1:8000/api/images';
+        let request_images = new XMLHttpRequest();
+        request_images.open('GET', requestURL_images);
+        request_images.responseType = 'json';
+        request_images.send();
+        request_images.onload = function() {
+            console.log(request_images.response);
+            images = request_images.response;
+            viewInit(trips, images);
+        }
+    }
+
+
     createSwiper();
     $('.categoryradio').change(function() {
         let val = $(this).val();
@@ -36,7 +62,6 @@ function init(){
         for(let i = 0; i < slides.length; i++){
             console.log(activeIndex + sign * i);
             if($(slides[activeIndex + sign * i]).css("display") != "none"){
-                // swiper.activeIndex=2
                 console.log("before"+activeIndex);
                 // swiper.slideTo(activeIndex+sign*i);
                 console.log(swiper.slideTo(2));
@@ -58,6 +83,8 @@ function init(){
     
     });
 }
+
+// ------------------------ createSwiper --------------------------------
 const createSwiper=()=>{
     swiper = new Swiper('#tripselectioncontainer', {
         // これをするとぎこちなさがなくなるらしい
@@ -68,9 +95,9 @@ const createSwiper=()=>{
         speed: 500,
         slidesPerView: 3,
         // spaceBetween: 40,
-        autoplay: {
-            delay: 3000,
-        },
+        // autoplay: {
+        //     delay: 3000,
+        // },
         navigation: {
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
@@ -80,23 +107,64 @@ const createSwiper=()=>{
             type:'bullets',
             clickable:true,
         },
-        // breakpoints: {
-
-        //     640: {
-        //         slidesPerView: 2.5,
-        //     },
-        //     768: {
-        //         slidesPerView: 2.75,
-        //     },
-        //     1080: {
-        //         slidesPerView: 3.25,
-        //     },
-        //     1280: {
-        //         slidesPerView: 3.75,
-        //     },
-        // },
     });
 }
+
+// -------------------------------- photoInit ----------------------------------------
+function photoInit(){
+    $(".photos").each(function(index, element){
+        console.log(this);
+        console.log($(this).height() / 4 + "px")
+        // $(this).css("grid-auto-columns", $(this).height() / 4 + "px");
+    });
+    $(".swiper-slide").each(function(index, element){
+        $(this).css("width", $(this).children(".photos").width());
+    });
+}
+// -----------------------------------------------------------------------------------
+
+// ------------------------------- viewInit -----------------------------------------
+function viewInit(trips, images){
+    for(let i = 0; i < trips.length; i++){
+        let swiperSlide = $($('#swiper-wrapper_template').html());
+        
+        // filter
+        // let extracted = images.filter(function(item){
+        //     return item.location == i;
+        // });
+
+        // swiper.removeAllSlides();
+        swiperSlide.children("a").children(".photo").children("img").attr("src",api_prefix+images[i].file_name);
+        swiperSlide.children("a").children(".tripDetail").children(".tripTitle").text(trips[i].trip_name);
+        swiperSlide.children("a").children(".tripDetail").children(".tripdate").text(trips[i].trip_start);
+        console.log(swiperSlide.get(0));
+        swiper.appendSlide(swiperSlide.get(0));
+        // swiper.appendSlide('<div class="swiper-slide"><img src="Bの画像" alt="lifethings"></div>');
+        // swiper.appendSlide('<div class="swiper-slide"><img src="Cの画像" alt="lifethings"></div>');
+
+        // console.log(extracted.length);
+        // if(extracted.length == 0)continue;
+        
+        // extracted.forEach(function(element){
+        //     console.log("importance: " + element.importance);
+        //     let photo = null;
+        //     if(element.importance > 5){
+        //         photo = $($('#photo_big_template').html());
+        //     }else{
+        //         photo = $($('#photo_small_template').html());
+        //     }
+        //     let img = new Image();
+        //     img.src = api_prefix + element.file_name;
+        //     $(img).appendTo(photo);
+        //     photo.appendTo(swiperSlide.children(".photos"));
+
+        // });
+        swiperSlide.appendTo('#locationHighliteWrapper');
+        $($('#spacer_template').html()).appendTo('#locationHighliteWrapper')
+    }
+    photoInit();
+}
+// -----------------------------------------------------------------------------------
 
 
 
